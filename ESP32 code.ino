f2f5080806b51309750a18bed10d9912a46ec0e0
Krutik <chodavadiyakrutik22@gmail.com>
2026-04-03 02:41:35 +0530

    Refactor to read and send sensor data from Serial
    Updated the code to read sensor data from Serial input and send it to ThingSpeak. Replaced hardcoded values with dynamic data parsing.

--- ESP32 code.ino
+++ ESP32 code.ino
@@ -3,9 +3,8 @@
 
 const char* ssid = "yourSSID";
 const char* password = "yourPASSWORD";
-
-unsigned long channelID = 2699677;
-const char* writeAPIKey = "0JNA7MB8ISFV44NM";
+unsigned long myChannelNumber = 2699677;
+const char* myWriteAPIKey = "0JNA7MB8ISFV44NM";
 
 WiFiClient client;
 
@@ -16,22 +15,41 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
+  Serial.println("WiFi connected");
+  
   ThingSpeak.begin(client);
 }
 
 void loop() {
-  // Dummy values for now
-  float temp = 25.0;
-  float hum = 60.0;
-  int people = 3;
-  int fanStatus = 1; // 1 = ON, 0 = OFF
+  if (Serial.available()) {
+    String data = Serial.readStringUntil('\n');
+    Serial.println("Received data: " + data); // Print received data
 
-  ThingSpeak.setField(1, temp);       // temperature
-  ThingSpeak.setField(2, hum);        // humidity
-  ThingSpeak.setField(3, people);     // people count
-  ThingSpeak.setField(4, fanStatus);  // fan status
+    int commaIndex1 = data.indexOf(',');
+    int commaIndex2 = data.indexOf(',', commaIndex1 + 1);
+    int commaIndex3 = data.indexOf(',', commaIndex2 + 1);
+    int commaIndex4 = data.indexOf(',', commaIndex3 + 1);
 
-  ThingSpeak.writeFields(channelID, writeAPIKey);
+    float temperature = data.substring(0, commaIndex1).toFloat();
+    float humidity = data.substring(commaIndex1 + 1, commaIndex2).toFloat();
+    int peopleCount = data.substring(commaIndex2 + 1, commaIndex3).toInt();
+    int fanStatus = data.substring(commaIndex3 + 1, commaIndex4).toInt();
+    int greenLedStatus = data.substring(commaIndex4 + 1).toInt();
 
-  delay(15000); // ThingSpeak minimum delay
+    Serial.print("People Present: ");
+    Serial.println(peopleCount);
+    Serial.print("Fan Status: ");
+    Serial.println(fanStatus ? "ON" : "OFF");
+    Serial.print("Light Status: ");
+    Serial.println(greenLedStatus ? "ON" : "OFF");
+
+    ThingSpeak.setField(1, temperature);
+    ThingSpeak.setField(2, humidity);
+    ThingSpeak.setField(3, peopleCount);
+    ThingSpeak.setField(4, fanStatus);
+    ThingSpeak.setField(5, greenLedStatus);
+    ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  }
+
+  delay(2000); // Update every 2 seconds
 }
